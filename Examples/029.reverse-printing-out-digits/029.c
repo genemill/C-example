284ebc246cc1d29c2546bf221c0e201107bd3164
genemill <fengjixuchui123@gmail.com>
2016-04-18 15:40:56 +0800

    Create 029.c

---
+++ Examples/029.reverse-printing-out-digits/029.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+   long a,b,c,d,e,x;
+   scanf("%ld",&x);
+   a=x/10000;/*分解出万位*/
+   b=x%10000/1000;/*分解出千位*/
+   c=x%1000/100;/*分解出百位*/
+   d=x%100/10;/*分解出十位*/
+   e=x%10;/*分解出个位*/
+   if(a!=0)printf("there are 5,%ld %ld %ld %ld %ld\n",e,d,c,b,a);
+   else if(b!=0)printf("there are 4,%ld %ld %ld %ld\n",e,d,c,b);
+   else if(c!=0)printf("there are 4,%ld %ld %ld\n",e,d,c);
+   else if(d!=0)printf("there are 4,%ld %ld\n",e,d);
+   else if(e!=0)printf("there are 4,%ld\n",e);
+   getch();
+}

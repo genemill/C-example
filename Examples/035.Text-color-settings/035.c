a3d861a1e55cd8648156d44019479adc7db2b1d9
genemill <fengjixuchui123@gmail.com>
2016-04-19 14:54:14 +0800

    Create 035.c

---
+++ Examples/035.Text-color-settings/035.c
@@ -0,0 +1,15 @@
+//TC中运行
+#include<stdio.h>
+#include<conio.h>
+void main(void)
+{
+    int color;
+    for(color=1;color<16;color++)
+    {
+        textcolor(color);
+        cprintf("This is color %d\r\n",color);
+    }
+    textcolor(128+15);
+    cprintf("This is blinking\r\n");
+    getch();
+}

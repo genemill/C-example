e321825f193352ac25e1d4e4cd486078a7865eca
genemill <fengjixuchui123@gmail.com>
2016-04-20 13:31:06 +0800

    Create 039.c

---
+++ Examples/039.Insert-array/039.c
@@ -0,0 +1,38 @@
+//运行不成功
+#include "stdio.h"
+#include "conio.h"
+main()
+{
+  int a[11]={1,4,6,9,13,16,19,28,40,100};
+  int temp1,temp2,number,end,i,j;
+  printf("original array is:\n");
+  for(i=0;i<10;i++)
+    printf("%5d",a);
+  printf("\n");
+  printf("insert a new number:");
+  scanf("%d",&number);
+  end=a[9];
+  if(number>end)
+    a[10]=number;
+  else
+  {
+    for(i=0;i<10;i++)
+    {
+      if(a>number)
+      {
+        temp1=a;
+        a=number;
+        for(j=i+1;j<11;j++)
+        {
+          temp2=a[j];
+          a[j]=temp1;
+          temp1=temp2;
+        }
+        break;
+      }
+    }
+  }
+  for(i=0;i<11;i++)
+    printf("%6d",a);
+  getch();
+}
